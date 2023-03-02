62697bb9adcff82f9ec491bc6abf4566c74274ef
tobyweir3 <BreakAt32>
2023-03-02 22:25:07 +0000

    added async storage and pedometer, both need installs using npm

--- App.js
+++ App.js
@@ -1,16 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-
+import { Pedometer } from 'expo-sensors';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 export default function App() {
   const [count, setCount] = useState(0);
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
+  const [pastStepCount, setPastStepCount] = useState(0);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
 
+  const _storeData = async () => {
+    console.log("trying to store")
+    try {
+      await AsyncStorage.setItem(
+        '@count',
+        count.toString()
+      );
+    } catch (error) {
+      // Error saving data
+      console.log("eror", error.message);
+    }
+  };
+  const _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@count');
+      if (value !== null) {
+        // We have data!!
+        console.log(parseInt("got value"))
+        setCount(parseInt(value))
+      }
+    } catch (error) {
+      console.log("error")
+      // Error retrieving data
+    }
+  };
   const incrementCount = () => {
     setCount(count + 1);
-  };
-
+    _storeData();
+  }
   const decrementCount = () => {
     setCount(count - 1);
+    _storeData();
   };
+  useEffect(() => {
+    async function retrieveData() {
+      await _retrieveData();
+    }
+    retrieveData();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -18,6 +54,8 @@ export default function App() {
       <View style={styles.buttonContainer}>
         <Button title="+" onPress={incrementCount} />
         <Button title="-" onPress={decrementCount} />
+        <Button title="clear" onPress={() => setCount(0)} />
+        <Button title="load"  onPress={_retrieveData} />
       </View>
     </View>
   );
