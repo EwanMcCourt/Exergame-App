ed3406ef3a7513f322dacfecf8e385a6c7e78371
Ewan <ewanmccourt32@gmail.com>
2023-03-02 11:57:24 +0000

    Login Screen Stuff

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View,Dimensions, Button,StatusBar, Image,ImageBackground,Platform,TextInput, SafeAreaView, TouchableNativeFeedback } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import LoginScreen from './app/screens/LoginScreen';
+
+export default function App() {
+ 
+  return <WelcomeScreen />;
+    
+  
+}
+
+
