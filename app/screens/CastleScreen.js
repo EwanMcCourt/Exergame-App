73a5be4948f401a38e4db38f78f064db94ba7d2c
Ewan <ewanmccourt32@gmail.com>
2023-03-11 19:29:06 +0000

    Scrollable Upgradge Tree

--- app/screens/CastleScreen.js
+++ app/screens/CastleScreen.js
@@ -1,49 +1,80 @@
 import React from 'react';
-import { StyleSheet, ImageBackground, View, Image, Text, Button,TouchableOpacity, TouchableHighlight } from 'react-native';
+import { StyleSheet, StatusBar,SafeAreaView,ImageBackground, View, Image, Text, Button,TouchableOpacity, TouchableHighlight } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const backgroundimage = {uri: "https://live.staticflickr.com/4242/35699339972_4ce24484ee_b.jpg"};
 const icon = {uri:"https://cdn3.iconfinder.com/data/icons/nature-animals/512/Bird-512.png"};
 
 
 function CastleScreen({navigation}) {
-    return (
-        <ImageBackground  
-        
-          source = {backgroundimage}
+    return (    
+<ImageBackground  source = {backgroundimage}
           fadeDuration={1000}
           
-          style = {styles.background}
-          >
-            <View style = {styles.titleContainer}>
-                <Image style = {styles.logo} source = {icon}/>
-                <Text> This is the castle screen </Text>
+         style={styles.container}>
+  <ScrollView style={styles.scrollView}>
+    <View style={styles.upgradeContainer}  >
+        <TouchableHighlight style = {styles.upgradeButton}>
+            <View>
+                <Text>Level 1</Text>
             </View>
-            
-        </ImageBackground> 
-    );
-}
+        </TouchableHighlight>
+        <TouchableHighlight style = {styles.upgradeButton}>
+            <View>
+                <Text>Level 2</Text>
+            </View>
+        </TouchableHighlight>
+        <TouchableHighlight style = {styles.upgradeButton}>
+            <View>
+                <Text>Level 3</Text>
+            </View>
+        </TouchableHighlight>
+        <TouchableHighlight style = {styles.upgradeButton}>
+            <View>
+                <Text>Level 4</Text>
+            </View>
+        </TouchableHighlight>
+        <TouchableHighlight style = {styles.upgradeButton}>
+            <View>
+                <Text>Level 5</Text>
+            </View>
+        </TouchableHighlight>
+    </View>    
+   </ScrollView>
+    </ImageBackground>
+  );
+};
 
 const styles = StyleSheet.create({
-    background:{
-        flex:1,
-        fadeDuration: 1000,
-        justifyContent: "flex-end",
-        alignItems:"center",
-        
-    },
-  
-    logo:{
-        width:100,
-        height:100,
-        alignItems:"center",
-        
-        
-        
-    },
-
-    titleContainer:{
-        position: "absolute",
-        top: 30,
-    },
-})
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    alignItems:"center",
+    
+  },
+  upgradeButton:{
+    width:"50%",
+    height: 125,
+    marginRight:"25%",
+    marginTop:"35%",
+    backgroundColor: "#d45404",
+    alignItems:"center",
+    justifyContent: "center",
+},
+upgradeContainer: {
+    width: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-end",
+    marginBottom:35,
+    
+  },
+  scrollView: {
+ 
+    
+  },
+  text: {
+    fontSize: 42,
+  },
+});
 export default CastleScreen;
