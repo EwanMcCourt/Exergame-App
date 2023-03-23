7d436e4c32fe4827d6d2569a6dc770f8afaf5765
tobyweir3 <BreakAt32>
2023-03-23 19:09:31 +0000

    almost there, pretty functional

--- app/screens/CastleScreen.js
+++ app/screens/CastleScreen.js
@@ -14,7 +14,6 @@ import {
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import * as Progress from "react-native-progress";
-import UpgradeList from "./UpgradeList";
 import ProgressBar from "./ProgressBar";
 import { useEffect, useState, useContext } from "react";
 import CountContext from "./CountContext";
@@ -33,7 +32,7 @@ function CastleScreen({ navigation }) {
   //     await AsyncStorage.removeItem("defenceProgress")
   // }
   const {count, setCount} = useContext(CountContext);
-  [currentProgresses, setProgress] = useState({
+  [currentProgress, setCurrentProgress] = useState({
     attack: 0,
     health: 0,
     defence: 0,
@@ -53,8 +52,34 @@ function CastleScreen({ navigation }) {
   const [hBTitle, setHBTitle] = useState("Buy");
   const [aBTitle, setABTitle] = useState("Buy");
   const [dBTitle, setDBTitle] = useState("Buy");
+  const upgradeHealth = () => {
+    if (count >= hCost){
+    setCount(count - hCost)
+    setHLevel(hLevel + 1);
+    setCurrentProgress({attack: currentProgress.attack,health: currentProgress.health + 0.25,defence: currentProgress.defence});
+  } 
+
+  };
+  const upgradeAttack = () => {
+    if (count >= aCost){
+      
+      setCount(count - aCost)
+      setALevel(aLevel + 1);
+      setCurrentProgress({attack: currentProgress.attack + 0.25 ,health: currentProgress.health,defence: currentProgress.defence});
+    }
+  };
+  const upgradeDamage = () => {
+    if (count >= dCost){
+      
+      setCount(count - dCost)
+      setDLevel(dLevel + 1);
+      setCurrentProgress({attack: currentProgress.attack,health: currentProgress.health,defence: currentProgress.defence + 0.25});
+    }
+  };
   useEffect(() => {
-    if (hLevel < 5 ){setHCost(hLevel*1000);} else 
+    if (hLevel < 5 ){
+      setHCost(hLevel*1000);
+    } else 
     {setHCost("Complete");
      setHButton(true);
      setHBTitle("Complete");}
@@ -77,18 +102,18 @@ function CastleScreen({ navigation }) {
       
       <ScrollView style={styles.scrollView}>
         {/* <Button onPress={clearStorage()} title = "clear"/> */}
-        <ProgressBar spec="health" currentProgress={currentProgresses.health} />
-        <ProgressBar spec="attack" currentProgress={currentProgresses.attack} />
+        <ProgressBar spec="health" currentProgress={currentProgress.health} />
+        <ProgressBar spec="attack" currentProgress={currentProgress.attack} />
         <ProgressBar
           spec="defence"
-          currentProgress={currentProgresses.defence}
+          currentProgress={currentProgress.defence}
         />
         <View style={{ backgroundColor: 'red', height: 100, width: 300, marginTop:20, }}>
           <Text style={{ color: 'white' }}>Health</Text>
           <Text style={{ color: 'white' }}>Cost: {hCost}</Text>
           <Text style={{ color: 'white' }}>Level: {hLevel}/5</Text>
           <View style={{ position: 'absolute', bottom: 0, right: 0 }}>
-            <Button title={hBTitle} onPress={() => setHLevel(hLevel + 1)} disabled = {hButton} />
+            <Button title={hBTitle} onPress={() => upgradeHealth()} disabled = {hButton} />
           </View>
         </View>
         <View style={{ backgroundColor: 'blue', height: 100, width: 300, marginTop:20, }}>
@@ -96,7 +121,9 @@ function CastleScreen({ navigation }) {
           <Text style={{ color: 'white' }}>Cost: {aCost}</Text>
           <Text style={{ color: 'white' }}>Level: {aLevel}/5</Text>
           <View style={{ position: 'absolute', bottom: 0, right: 0 }}>
-            <Button title={aBTitle} onPress={() => setALevel(aLevel + 1)} disabled = {aButton} />
+            <Button title={aBTitle} onPress={() => 
+              upgradeAttack()
+              } disabled = {aButton} />
           </View>
         </View>
         <View style={{ backgroundColor: 'purple', height: 100, width: 300, marginTop:20, }}>
@@ -104,14 +131,13 @@ function CastleScreen({ navigation }) {
           <Text style={{ color: 'white' }}>Cost: {dCost}</Text>
           <Text style={{ color: 'white' }}>Level: {dLevel}/5</Text>
           <View style={{ position: 'absolute', bottom: 0, right: 0 }}>
-            <Button title={dBTitle} onPress={() => setDLevel(dLevel + 1)} disabled = {dButton} />
+            <Button title={dBTitle} onPress={() => upgradeDamage()} disabled = {dButton} />
           </View>
         </View>
       </ScrollView>
     </ImageBackground>
   );
-}
-
+            }
 const styles = StyleSheet.create({
   text: {
     fontSize: 40,
